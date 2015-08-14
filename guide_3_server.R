73d4303a055aee993d48244434b8d2c0e595b632
shengreen <shengreen@hotmail.com>
2015-08-14 15:07:39 +0800

    Update guide_3_server.R

--- guide_3_server.R
+++ guide_3_server.R
@@ -1,13 +1,27 @@
 library(shiny)
 
+# options(expressions=10000)
+
 shinyServer(function(input, output){
   data <- reactive({
-    dist <- switch(input$dist, norm = rnorm, unif = rlnorm, exp = rexp, rnorm)
-    cat("dist: ", dist, "\n")
-    cat("input$n", input$n, "\n")
-    dist(input$n)
+    mydist <- switch(input$dist, norm = rnorm, unif = rlnorm, exp = rexp, rnorm)
+    cat("input$dist: ", input$dist, "\n")
+    cat("input$n: ", input$n, "\n")
+    cat("typeof(mydist): ", typeof(mydist), "\n")
+    cat("mydist(input$n): ", mydist(input$n), "\n")
+    mydist(input$n)
+  })
+  
+  output$plot <- renderPlot({
+    mydist <- input$dist
+    n <- input$n
     
-    hist(data(), main=paste('r', dist, '(', n, ')', sep=''))
+    cat("main: ", paste('r', mydist, '(', n, ')', sep=''), "\n")
+    temp <- data()
+    cat("temp: ", temp, "\n")
+    
+    hist(data(), 
+         main=paste('r', mydist, '(', n, ')', sep=''))
   })
   
   output$summary <- renderPrint({
