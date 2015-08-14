94b5b2229c7484c360d562d2405529bb4cdfb953
shengreen <shengreen@hotmail.com>
2015-08-14 15:42:11 +0800

    Create guide_4_server.R

---
+++ guide_4_server.R
@@ -0,0 +1,16 @@
+library(shiny)
+
+shinyServer(function(input, output) {
+  datasetInput <- reactive({
+    switch(input$dataset, "rock"=rock, "pressure"=pressure, "cars"=cars)
+  })
+  
+  output$summary <- renderPrint({
+    dataset <- datasetInput()
+    summary(dataset)
+  })
+  
+  output$view <- renderTable({
+    head(datasetInput(), n = input$obs)
+  })
+})
